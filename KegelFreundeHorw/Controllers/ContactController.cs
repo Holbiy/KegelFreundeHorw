8e23715c3755ffd84ce56cca46db0815e884143d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-02 21:31:32 +0200

    mail

---
+++ KegelFreundeHorw/Controllers/ContactController.cs
@@ -0,0 +1,95 @@
+using System;
+using KegelFreundeHorw.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+
+namespace KegelFreundeHorw.Controllers
+{
+	public class ContactController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IConfiguration _configuration;
+
+		public ContactController(ILogger<HomeController> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			_configuration = configuration;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult SendMail(MailFormModel model){
+			var fromAddress = new MailAddress(_configuration.GetValue<string>("Mail:Address"), model.Name);
+			var toAddress = new MailAddress(_configuration.GetValue<string>("Mail:Address"), "Kegel Freunde Horw");
+			string fromPassword = _configuration.GetValue<string>("Mail:Password");
+			string subject = model.Subject;
+			string body = getContent(model);
+			if (string.IsNullOrEmpty(body))
+			{
+				ViewBag.ErrorMessage = "Mail konnte nicht versendet werden.";
+				return View("~/Views/Contact/Index.cshtml");
+			}
+
+			var smtp = new SmtpClient
+			{
+				Host = "smtp.gmail.com",
+				Port = 587,
+				EnableSsl = true,
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				UseDefaultCredentials = false,
+				Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+			};
+			using (var message = new MailMessage(fromAddress, toAddress)
+			{
+				Subject = subject,
+				Body = body,
+				IsBodyHtml = true
+			})
+			{
+				try
+				{
+					smtp.Send(message);
+				}
+				catch (Exception e)
+				{
+					ViewBag.ErrorMessage = "Mail konnte nicht versendet werden.";
+					return View("~/Views/Contact/Index.cshtml");
+				}
+			}
+
+			return View("~/Views/Contact/Success.cshtml");
+		}
+
+		private string getContent(MailFormModel model)
+		{
+			string body;
+			try
+			{
+				using (StreamReader reader = new StreamReader(_configuration.GetValue<string>("Mail:ContentPath")))
+				{
+					body = reader.ReadToEnd();
+				}
+			}
+			catch (Exception e)
+			{
+				return null;
+			}
+
+			body = body.Replace("!content!", model.Content);
+			body = body.Replace("!name!", model.Name);
+			body = body.Replace("!mail!", model.Sender);
+
+			return body;
+
+		}
+
+	}
+}
---
+++ KegelFreundeHorw/Models/MailFormModel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KegelFreundeHorw.Models{
+    public class MailFormModel{
+        [StringLength(50, ErrorMessage = "Betreff darf maximal {1} Zeichen beinhalten.")]
+        [Required(ErrorMessage = "Betreff muss angegeben werden.")]
+        [Display(Name = "Betreff")]
+        public string Subject { get; set; }
+
+        [StringLength(50, ErrorMessage = "Name darf maximal {1} Zeichen beinhalten.")]
+        [Required(ErrorMessage = "Name muss angegeben werden")]
+        [RegularExpression("^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)", ErrorMessage = "Erlaubte Zeichen: (A-Z) (a-z) (' space -)")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email muss angegeben werden.")]
+        [EmailAddress(ErrorMessage = "Falsches Format.")]
+        [Display(Name = "E-Mail Adresse")]
+        public string Sender { get; set; }
+        
+        [Required(ErrorMessage = "Nachricht muss angegeben werden.")]
+        [StringLength(500, ErrorMessage = "Inhalt darf nicht mehr als {1} Zeichen beinhalten.")]
+        [Display(Name = "Nachricht")]
+        public string  Content { get; set; }
+
+
+    }
+
+}
